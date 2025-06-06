509f8d540f023a06a982cc08a4853df2207d489f
sak1rro <91954858+sak1rro@users.noreply.github.com>
2025-06-06 13:45:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,89 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int precedence(char op) {
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
+  return 0;
 }
 
+std::string infx2pstfx(const std::string& inf) {
+ {
+  TStack<char, 100> stack;
+  std::string result;
+
+  for (size_t i = 0; i < inf.length(); ++i) {
+    char token = inf[i];
+
+    if (isdigit(token)) {
+      while (i < inf.length() && isdigit(inf[i])) {
+        result += inf[i];
+        ++i;
+      }
+      --i;
+      result += ' ';
+    }
+
+    else if (token == '(')
+      stack.push(token);
+
+    else if (token==')') {
+      while (!stack.isEmpty() && stack.top() != '(') {
+        result += stack.pop();
+        result += ' ';
+      }
+      stack.pop();
+    }
+
+    else if (token == '+' || token == '-' || token == '*' || token == '/') {
+      while (!stack.isEmpty() && precedence(stack.top()) >= precedence(token)) {
+        result += stack.pop();
+        result += ' ';
+      }
+      stack.push(token);
+    }
+  }
+
+  while (!stack.isEmpty()) {
+    result += stack.pop();
+    result += ' ';
+  }
+
+  if (!result.empty())
+    result.pop_back();
+
+  return result;
+}
+
+
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 100> stack;
+  std::string num;
+
+  for (size_t i = 0; i < post.length(); ++i) {
+    char token = post[i];
+
+    if (token == ' ') continue;
+
+    if (isdigit(token)) {
+      num.clear();
+      while (i < post.length() && isdigit(post[i])) {
+        num += post[i];
+        ++i;
+      }
+      --i;
+      stack.push(std::stoi(num));
+    }
+    else {
+      int b = stack.pop();
+      int a = stack.pop();
+      switch (token) {
+      case '+':stack.push(a + b); break;
+      case '-':stack.push(a - b); break;
+      case '*':stack.push(a + b); break;
+      case '/':stack.push(a / b); break;
+      }
+    }
+  }
+  return stack.pop();
 }
