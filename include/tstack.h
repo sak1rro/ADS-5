01d15d026f288670b6c5d2aec963a4b38b17ec6c
sak1rro <91954858+sak1rro@users.noreply.github.com>
2025-06-06 13:40:32 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,32 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T data[size];
+  int topIndex;
+
+public:
+  TStack(): topIndex(-1) {}
+
+  bool isEmpty()const { return topIndex == -1; }
+
+  void push(const T& value) {
+    if (topIndex >= size)
+      throw std::overflow_error("Stack overflow");
+    data[++topIndex] = value;
+  }
+
+  T pop() {
+    if (isEmpty())
+      throw std::underflow_error("Stack underflow");
+    return data[topIndex--];
+  }
+
+  T top()const{
+    if (isEmpty())
+      throw std::underflow_error("Stack is empty");
+    return data[topIndex];
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
